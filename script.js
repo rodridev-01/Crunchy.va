efc1e6187969be655be94fbf6bfcbeddc5e42263
XrL8D <rph26031994@gmail.com>
2024-04-28 01:21:19 -0500

    +video

--- script.js
+++ script.js
@@ -54,3 +54,9 @@ var swiper = new Swiper(".coming-container", {
     }
   });
 
+function toggleVideo(){
+  const trailer=document.querySelector('.trailer');
+  const video=document.querySelector('video');
+  video.pause();
+  trailer.classList.toggle('active')
+}
